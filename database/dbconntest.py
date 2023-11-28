f68bc51f73cc2100f4140323bfbb731585052caa
abruce3411 <abruce3411@gmail.com>
2023-11-28 12:04:38 -0600

    db conn test updates

---
+++ database/dbconntest.py
@@ -0,0 +1,24 @@
+from psycopg2 import *
+import json, os
+import dotenv
+
+
+def db_test():
+    load_dotenv()
+    try:
+        connection = psycopg2.connect(
+            database=os.getenv("database"),
+            host=os.getenv("dbhost"),
+            user=os.getenv("user"),
+            password=os.getenv("password"),
+        )
+        return connection
+    except (Exception, psycopg2.Error) as error:
+        print("Error connection to PostgreSQL")
+        return None
+
+
+if __name__ == "__main__":
+    conn = db_test()
+    if conn:
+        print("Success")
